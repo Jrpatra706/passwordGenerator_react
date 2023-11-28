99e0605110da36ab1701e8627157b0bf28e7d23a
Jyotiranjan Patra <112277029+Jrpatra706@users.noreply.github.com>
2023-11-28 23:31:34 +0530

    done with the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import './App.css'
 
 function App() {
@@ -7,6 +7,8 @@ function App() {
   const [charsAllowed,setChars] = useState(false);
   const [password,setPassword] = useState('');
   
+  const passwdRef = useRef(null);
+
   const passwordGenerator=useCallback(()=>{
     let pass="";
     let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
@@ -30,20 +32,25 @@ function App() {
 
   return (
     <>
-      <div className='container  w-full max-w-md max-auto shadow-md rounded-lg px-4 my-10 text-orange-500 bg-gray-700'>
+      <div className='container  w-full max-w-md max-auto shadow-md rounded-lg px-4 my-10 '>
         <h1 className='text-white text-center my-3'>Password Generator</h1>
         <div className='flex shadow rounded-lg overflow-hidden mb-4'>
-          <input type="text" value={password} className='outline-none w-full py-1 px-3' placeholder='Password' readOnly />
-          <button className='outline-none bg-blue-700 text-white shrink-0 px-2 py-0.5' >
+          <input ref={passwdRef} type="text" value={password} className='text-black outline-none w-full py-1 px-3' placeholder='Password' readOnly />
+          <button
+          onClick={()=>{
+            passwdRef.current?.select();
+            window.navigator.clipboard.writeText(passwdRef.current.value);
+          }}
+           className='copy-btn outline-none bg-blue-700 text-white shrink-0 px-2 py-0.5' >
             Copy
           </button>
         </div>
-        <div className='flex text-sm gap-x-2 mb-4'>
+        <div className='text-white flex text-sm gap-x-2 mb-4'>
           <div className='flex items-center gap-x-1'>
-            <input type="range" min={6} max={100} 
+            <input type="range" min={6} max={35} 
             value={length} className='cursor-pointer' 
             onChange={(e)=>{setLength(e.target.value)}}/>
-            <label>Length({length})</label>
+            <label className='text-white'>Length({length})</label>
           </div>
           <div className='flex items-center gap-x-1'>
             <input type="checkbox" defaultChecked={numsAllowed} id='numberInput' 
